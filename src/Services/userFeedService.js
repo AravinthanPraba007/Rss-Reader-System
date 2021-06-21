47b514c78eab18cb0a58ae028b4d83f047ed39fe
Aravinthan <aravinthanpro@gmail.com>
2021-06-21 23:59:09 +0530

    Doing refactoring

--- src/Services/userFeedService.js
+++ src/Services/userFeedService.js
@@ -1,27 +1,27 @@
 const { QueryTypes, Op } = require('sequelize');
-const { UserRssSubscription,RssFeed, sequelize } = require('../../models');
+const { UserRssSubscription, RssFeed, sequelize } = require('../../models');
 const StatusMessage = require('../Constants/statusMessages');
 
 module.exports.getUserFeed = async function (userData) {
     try {
-        let offsetValue = (userData.page -1)*10;
+        let offsetValue = (userData.page - 1) * 10;
         let response = {
-            isUserFeedsFetched : false,
+            isUserFeedsFetched: false,
             userFeeds: []
         };
         let rssSiteList = [];
-        rssSiteList = await sequelize.query('SELECT rss_id FROM user_rss_subscription where user_id = :userId AND "deletedAt" IS NULL', 
-        {
-            replacements: { userId: userData.userId },
-            type: QueryTypes.SELECT 
-          });
-          let rssSiteIds = [];
-          rssSiteList.map((rssSite) => {
+        rssSiteList = await sequelize.query('SELECT rss_id FROM user_rss_subscription where user_id = :userId AND "deletedAt" IS NULL',
+            {
+                replacements: { userId: userData.userId },
+                type: QueryTypes.SELECT
+            });
+        let rssSiteIds = [];
+        rssSiteList.map((rssSite) => {
             rssSiteIds.push(rssSite.rss_id);
-          })
+        })
         let feeds = await RssFeed.findAndCountAll({
             where: {
-                rss_id : { [Op.in]: rssSiteIds }
+                rss_id: { [Op.in]: rssSiteIds }
             },
             order: [["createdAt", "DESC"]],
             offset: offsetValue,
