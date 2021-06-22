81b3117b4497d7af1750f167c9d3a88f96eaad69
Aravinthan <aravinthanpro@gmail.com>
2021-06-22 10:24:55 +0530

    Doing refactoring

--- src/DbModels/index.js
+++
@@ -1,8 +0,0 @@
-const Database = require('../Database/index');
-const Sequelize = require('sequelize');
-
-const db = {};
-db.Sequelize = Sequelize;
-db.sequelize = Database.sequelize;
-db.userModel = require('./user')(Database, Sequelize);
-module.exports = db;
