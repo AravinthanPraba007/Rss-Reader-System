f9dd16f5a1bb570325d35a8bdea10a03293d7fd4
Aravinthan <aravinthanpro@gmail.com>
2021-06-30 14:06:47 +0530

    Removed unwanted files

--- src/Helpers/feedReaderHelper2.js
+++
@@ -1,8 +0,0 @@
-// Currently not using this rss parser 
-let Parser = require('rss-parser');
-let parser = new Parser();
-
-module.exports.rssParser = async function (feedUrl) {
-    let feed = await parser.parseURL(feedUrl);
-    return feed;
-}
